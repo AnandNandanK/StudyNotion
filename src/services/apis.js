5e48c0bc41126baab8450b936d20850c7a8309f6
AnandNandanK <anandwising@gmail.com>
2024-08-14 13:22:13 +0530

    adding render backend URL in the apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,6 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = process.env.BASE_URL || "https://studynotion-backend-02.onrender.com/api/v1"
+
+// "http://localhost:4000/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
