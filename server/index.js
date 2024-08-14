fc81397609dabdf2a24d0370dfa242c8e3fe6dc8
AnandNandanK <anandwising@gmail.com>
2024-08-14 13:39:44 +0530

    Pasting vercel origin in index.js server

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "*",
+		origin: "*" || 'https://study-notion-git-main-anand-huklas-projects.vercel.app',
 		credentials: true,
 	})
 );
