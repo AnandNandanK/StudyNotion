1869418a6f02cf974698da8a40d45d64dd0a8027
AnandNandanK <anandwising@gmail.com>
2024-09-25 15:59:16 +0530

    changing origin

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://study-notion-teal-five.vercel.app/" || "*" ,
+		origin: "*" ,
 		credentials: true,
 	})
 );
