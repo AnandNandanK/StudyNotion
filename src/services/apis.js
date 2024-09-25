6223b7fe49a5e78e99183a9a1e646886c92bf9ed
AnandNandanK <anandwising@gmail.com>
2024-09-25 15:54:25 +0530

    removing /

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "https://studynotion-backend-02.onrender.com/api/v1/" 
+const BASE_URL = "https://studynotion-backend-02.onrender.com/api/v1" 
 
 
 // const BASE_URL ="http://localhost:4000/api/v1"
